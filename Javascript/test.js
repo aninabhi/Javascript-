cb3fa16b1704dae4fe17d4faf14f5e0ef687a5b8
*anintiqa* <*abhi199131@gmail.com*>
2024-02-15 17:57:51 +0530

    First Version

---
+++ Javascript/test.js
@@ -0,0 +1,18 @@
+const accountId = 144553 
+let accountEmail = "abis1182@gmail.com"
+var accountPassword = "123544"
+accountCity =  "Jaipur"
+
+accountEmail = "absd@gmail.com"
+accountPassword = "11254"   
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// accountId = 2
+
+
+
+
+
